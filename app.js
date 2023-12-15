48057551128f08fe0fbd5b126003e8348312091f
andresbf94 <andresbf94@hotmail.com>
2023-12-15 22:44:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');                 // Importa la biblioteca de Express
-const bodyParser = require('body-parser');
 const cors = require('cors');                       // Importa la biblioteca CORS
 require('dotenv').config();                         // Importa y configura la biblioteca dotenv para cargar las variables de entorno
 require('./config/db');                             // Conexion a la base de datos
@@ -7,7 +6,6 @@ require('./config/db');                             // Conexion a la base de dat
 const app = express();                              // Crea el servidor
 
 app.use(cors());                                    // Condigura el Middleware CORS en la aplicación, permitiendo solicitudes de cualquier origen
-app.use(bodyParser.raw({ type: 'application/json' }));                                  
 app.use(express.json());                            // Configuracion para poder recivir archivos json y tratarlos como objetos
 app.use(express.urlencoded({extended: false}));     // Configura el middleware para analizar los cuerpos de las solicitudes codificados en URL
 app.use('/uploads', express.static('uploads'));     // Configura la ruta /uploads para servir archivos estaticos desde el dirctorio "uploads"
